1648bf17bbb65d8adf1604e30353fa9898190c2d
Ujjval Pateliya <154922422+ujjvalap@users.noreply.github.com>
2025-09-15 17:28:51 +0530

    Complted

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://neuromart-server.onrender.com/'
+  server: {
+    proxy: {
+      '/api': {                  // Ye frontend ke /api requests ko intercept karega
+        target: 'https://neuromart-server.onrender.com',
+        changeOrigin: true,      // Backend ko request ke origin ko frontend ki tarah dikhaega
+        secure: true,            // HTTPS backend ke liye
       }
     }
   }
