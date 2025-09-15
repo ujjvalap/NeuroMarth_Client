11a4850999eac8ac8332be215c1318554304541c
Ujjval Pateliya <154922422+ujjvalap@users.noreply.github.com>
2025-09-15 17:23:15 +0530

    fix issue  server url

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target:'http://localhost:5000'
-//       }
-//     }
-//   }
-// })
-
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // env variables load karo
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL, // env file se value
-          changeOrigin: true,
-        }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    proxy:{
+      '/api':{
+        target:'https://neuromart-server.onrender.com/'
       }
     }
   }
 })
+
