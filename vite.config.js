c88886461e0e134f2473ce5e83e0d75a47417eae
Ujjval Pateliya <154922422+ujjvalap@users.noreply.github.com>
2025-09-16 21:41:44 +0530

    Fix issue

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://neuromart-server.onrender.com',
-        changeOrigin: true,
-        secure: true,
-      }
-    }
-  },
-  build: {
-    sourcemap: true, // optional: error trace original file me milega
-  }
 })
